92c5c5d3bc1d180d0981fcd017fc5dbbac190400
ichtrojan <ordrizzy@gmail.com>
2020-05-23 23:06:07 +0100

    cleanup, removed authenticable and updated fillables

---
+++ app/Models/User.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @method static create(array $user)
+ */
+class User extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'username', 'phone', 'website', 'id'
+    ];
+}
