284ab3966ded8e56a8eed4217b5f0d2622cf8fcd
ichtrojan <ordrizzy@gmail.com>
2020-05-23 23:08:22 +0100

    added columns according to JSONPlaceholder

--- database/migrations/2020_05_23_202141_create_comments_table.php
+++ database/migrations/2020_05_23_202141_create_comments_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateCommentsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('comments', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('post_id');
             $table->string('name');
-            $table->string('username');
             $table->string('email');
-            $table->string('phone');
-            $table->string('website');
+            $table->text('body');
+            $table->foreign('post_id')->references('id')->on('posts');
             $table->timestamps();
         });
     }
@@ -31,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('comments');
     }
 }
