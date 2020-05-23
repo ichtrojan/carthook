cdedd21dce4bdcfcbc72ca10e357782a9c671a01
ichtrojan <ordrizzy@gmail.com>
2020-05-24 00:55:11 +0100

    The routes

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['middleware' => 'api'], function () {
+    Route::group(['prefix' => 'users'], function () {
+        Route::get('/', 'UserController@all');
+        Route::get('{user}', 'UserController@get');
+        Route::get('{user}/posts', 'UserController@posts');
+        Route::get('{user}/posts/{post}/comments', 'PostController@comments');
+    });
 });
