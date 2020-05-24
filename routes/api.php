ccfb0ace2e865fece32e7d7f6c4e0455a5fc712a
ichtrojan <ordrizzy@gmail.com>
2020-05-24 03:21:44 +0100

    cleanup

--- routes/api.php
+++ routes/api.php
@@ -13,11 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::group(['middleware' => 'api'], function () {
-    Route::group(['prefix' => 'users'], function () {
-        Route::get('/', 'UserController@all');
-        Route::get('{user}', 'UserController@get');
-        Route::get('{user}/posts', 'UserController@posts');
-        Route::get('{user}/posts/{post}/comments', 'PostController@comments');
-    });
+Route::group(['middleware' => 'api', 'prefix' => 'users'], function () {
+    Route::get('/', 'UserController@all');
+    Route::get('{user}', 'UserController@get');
+    Route::get('{user}/posts', 'UserController@posts');
+    Route::get('{user}/posts/{post}/comments', 'PostController@comments');
 });
